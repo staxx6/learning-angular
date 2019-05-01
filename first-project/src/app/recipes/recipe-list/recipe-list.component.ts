01b411b904169d8f28bebc5f4a8beca2f8ecf86f
Simon Oczadly <staxx@hotmail.de>
2019-05-01 18:57:18 +0200

    update

---
+++ first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test', 'DescTest', 'https://cdn.pixabay.com/photo/2017/11/08/15/34/recipe-2930786_960_720.jpg'),
+    new Recipe('Test', 'DescTest', 'https://cdn.pixabay.com/photo/2017/11/08/15/34/recipe-2930786_960_720.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
